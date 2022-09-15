c6dffb079b5176b9dfa18ec16d6ee8e4ecf3da37
omar alshobky <93231975za@gmail.com>
2022-09-15 14:48:33 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var firstName="Hello\nOmar Alshobky\nthis my name"
+    var fullName: String=""
+    var intNam:Int=32
+    println(fullName.isEmpty())
+    println(intNam)
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println(firstName)
+}
