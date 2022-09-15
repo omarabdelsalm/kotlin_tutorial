02fb56e574049d58583f24c070477f64b3d67ebe
omar alshobky <93231975za@gmail.com>
2022-09-15 15:05:20 +0200

    خىث اادد

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ fun main() {
     // دالة الاخراج او الطباعة
     println(fullName.isEmpty())
     println(intNam)
+    // دالة الاخراج او الطباعة
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
